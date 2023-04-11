b7b832e49a3e9614549698e4c475d526116b35bb
ZenKhalil <feattzen@gmail.com>
2023-04-11 22:57:54 +0200

    Code created

---
+++ app.js
@@ -0,0 +1,46 @@
+"use strict";
+
+// Et array med 40 elementer der er tomme
+const queue = new Array(40).fill(0);
+
+// Funktion der opdaterer kø-diagrammet
+function updateChart() {
+  // Hent antal kunder fra en funktion
+  const queueSize = getNumberOfCustomers();
+
+  // Tilføj antal kunder til køen
+  queue.push(queueSize);
+
+  // Vis antal kunder og hele køen i konsollen (For at jeg kunne få et bedre overblik)
+  console.log(`Queue size: ${queueSize}, queue: ${queue}`);
+
+  // Hvis køen er længere end 40, fjern det ældste element
+  if (queue.length > 40) {
+    queue.shift();
+  }
+
+  // Hent alle kø-søjler og sæt højden på hver enkelt baseret på køens elementer
+  const bars = document.querySelectorAll(".queue-bar");
+  for (let i = 0; i < queue.length; i++) {
+    const height = (queue[i] / 32) * 100;
+    bars[i].style.height = `${height}%`;
+  }
+}
+
+// Find kø-diagrammet på siden
+const queueChart = document.getElementById("queue-chart");
+
+// Opret 40 kø-søjler og tilføj dem til kø-diagrammet (Enten det eller manuelt oprette 40 div i html)
+for (let i = 0; i < 40; i++) {
+  const queueBar = document.createElement("div");
+  queueBar.className = "queue-bar";
+  queueChart.appendChild(queueBar);
+}
+
+// Opdater kø-diagrammet hvert sekund
+setInterval(updateChart, 1000);
+
+// Funktion der giver et tilfældigt antal kunder
+function getNumberOfCustomers() {
+  return Math.floor(Math.random()*32);
+}
